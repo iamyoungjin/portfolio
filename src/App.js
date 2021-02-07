762b77397809c78a3e730b2951baf8165c8e9709
iamyoungjin <nnna08@likelion.org>
2021-02-07 19:27:43 +0900

    added Menubar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import logo from './logo.svg';
 import styled from 'styled-components';
-import './App.css';
 import { Component } from 'react';
-import Menu from './components/menu/menu'
-const Container = styled.div`
-  text-align: center;
-`;
+import Menubar from './hoc/Menu/Menubar'
+//import Menu from './components/menu/menu'
 
-//stateful component
 
-//stateless component
-const MyComponent = props => {
-return <h1>Welcome To my {props.text} {props.children}</h1>;
-};
 class App extends Component{
   render() {
     return (
-      //JSX (Javascript XML)
-      //text-aline:center (javascript object는 '-'이 들어갈 수 없음)
-      //inline 방법 , style방법, styled-components
-      <Container>
-      <MyComponent text={"ABC"} />
-      <div className='intro' 
-        style = {{textAlign:'center',backgroundColor:"blue"}}> 
-        <MyComponent text={"ABC"} />
-        <MyComponent text={"haha"}>
-          <h3>여기 commit test</h3>
-        </MyComponent>
-        iamyoungjin
-        <Menu/>
-      </div>
-      </Container>
+      <div><Menubar/></div>
     );
   }
 }
